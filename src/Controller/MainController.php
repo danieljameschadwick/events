d502a013853d8d05794f9bfa9f9de321d6db8af4
Daniel Chadwick <djchadwick@outlook.com>
2020-09-21 22:07:00 +0100

    EVNT payum support, downgraded Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Enumeration\NavigationEnumerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
--- src/Enumeration/NavigationEnumerator.php
+++ src/Enumeration/NavigationEnumerator.php
@@ -1,27 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Enumeration;
 
 class NavigationEnumerator
 {
-    public static $navigation = [
-        0 => [
-            'name' => 'Home',
-            'handle' => 'HOME',
-            'link' => '#',
-            'icon' => null,
-        ],
-        1 => [
-            'name' => 'News',
-            'handle' => 'NEWS',
-            'link' => '#',
-            'icon' => null,
-        ],
-        2 => [
-            'name' => 'Extra Tab',
-            'handle' => 'EXTRA_TAB',
-            'link' => '#',
-            'icon' => null,
-        ],
-    ];
+    public static $navigation = []; // todo: unrequired nav for now, implement in CMS
 }
