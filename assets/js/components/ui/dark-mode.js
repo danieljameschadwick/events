033c7598cedda9ed68465f1f455c8e20e7a97424
Daniel Chadwick <djchadwick@outlook.com>
2020-11-23 21:26:04 +0000

    EVNT implementing dark theme

---
+++ assets/js/components/ui/dark-mode.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html');
+
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        html.classList.add('dark');
+        document.querySelector('#theme-icon').classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
+
+    // Whenever the user explicitly chooses to respect the OS preference
+    // localStorage.removeItem('theme')
+});
+
+document.querySelector('#theme-icon').addEventListener('click', (event) => {
+    const target = event.currentTarget;
+    const html = document.querySelector('html');
+
+    console.log(target);
+
+    if (target.classList.contains('dark')) {
+        target.classList.remove('dark');
+        target.classList.add('light');
+
+        html.classList.remove('dark');
+
+        localStorage.theme = 'light';
+
+        console.log(localStorage.theme);
+        return;
+    }
+
+    if (target.classList.contains('light')) {
+        target.classList.remove('light');
+        target.classList.add('dark');
+
+        html.classList.add('dark');
+
+        localStorage.theme = 'dark';
+
+        console.log(localStorage.theme);
+        return;
+    }
+
+    target.classList.add('light');
+});
--- assets/js/entry_points/app.js
+++ assets/js/entry_points/app.js
@@ -8,3 +8,4 @@ const $ = require('jquery');
 // base
 import '../components/ui/dropdown';
 import '../components/ui/editor';
+import '../components/ui/dark-mode';
